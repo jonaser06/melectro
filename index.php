0a01d69f551b228bcfbd20a60ddf9ac6ac3154f6
Jonathan Narvaez <jonaser06@gmail.com>
2019-05-23 00:53:09 -0500

    maqueta login, dash board

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /* Controllers */
 require_once 'controller/home.controller.php';
 /* Models */
-require_once 'controller/home.model.php';
+require_once 'model/home.model.php';
 
 $home = new HomeController();
 $home->inicio();
